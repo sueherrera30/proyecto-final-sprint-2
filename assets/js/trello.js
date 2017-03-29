04c971edc6df80bfc47638032214a72b0aa07576
sue martinez <sueherrera30@gmail.com>
2017-03-29 12:42:55 +0000

    agrega listas con titulos y botones propios,pero elementos los agrega arriba de titulo

--- assets/js/trello.js
+++ assets/js/trello.js
@@ -1,26 +1,42 @@
 
-function agregarLista(){
-    var inputTexto = document.createElement("input");
-    inputTexto.setAttribute("type", "text");
-    inputTexto.setAttribute("placeholder", "agrega pendiente");
-    document.body.appendChild(inputTexto);
-    var boton = document.createElement("button");
-    var textoBoton = document.createTextNode("agregar");
-    boton.appendChild(textoBoton);
-    document.body.appendChild(boton);
-    boton.onclick = function agregarPendientes(){
+function crearLista(){
+   var nuevo = document.createElement("p");
+   nuevo.setAttribute("id","titulo");
+   nuevo.style.fontWeight="bold";
+   nuevo.style.fontSize="150%";
+   var tituloValor = document.getElementById('lista');
+   nuevo.innerText = tituloValor.value;
+   var almancen = document.getElementById("almacenGeneral");
+   almancen.appendChild(nuevo);
+
+  // // var input = document.getElementById('lista').value;
+  // // var titulo = document.createElement('p');
+  // // var textTitulo =document.createTextNode(input);
+  // // document.getElementById('titulo').appendChild(textTitulo)
+  //
+  var inputTexto = document.createElement("input");
+  inputTexto.setAttribute("type", "text");
+  inputTexto.setAttribute("placeholder", "agrega pendiente");
+  almancen.appendChild(inputTexto);
+  var boton = document.createElement("button");
+  var textoBoton = document.createTextNode("agregar");
+  boton.appendChild(textoBoton);
+  almancen.appendChild(boton);
+  // document.body.appendChild(boton);
+  boton.onclick = function agregarPendientes(){
       var inputPendiente =inputTexto.value;
       var elementoNuevoLista = document.createElement('LI');
       var pendiente = document.createTextNode(inputPendiente);
       elementoNuevoLista.appendChild(pendiente);
-      document.getElementById('listaGeneral').appendChild(elementoNuevoLista);
+      var listaGeneral =  document.getElementById('listaGeneral')
+      listaGeneral.appendChild(elementoNuevoLista);
+
+
     }
+
 }
 
-  function agregar(){
-    var input = document.getElementById('pendiente').value;
-    var titulo = document.createElement('p');
-    var textTitulo =document.createTextNode(input);
-    document.getElementById('titulo').appendChild(textTitulo);
-    agregarLista();
-    }
+// contenedor.insertBefore(nuevo,contenedor.firstChild)
+// function agregarNuevaLista(){
+//
+// }
